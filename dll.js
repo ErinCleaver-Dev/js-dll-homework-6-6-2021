6609424102ff880e79355eeb97b865ce3deeb99c
ErinCleaver-GRCC <erincleaver@email.grcc.edu>
2021-06-06 19:35:05 -0400

    1 file modified

--- dll.js
+++ dll.js
@@ -74,9 +74,21 @@ export default class DoubleLinkedList {
    * Add a node to the head of the linked list.
    */
   unshift(value) {
+    // used to add a new node
+    const newNode = new Node(value)
+    
+    // verifiy that the list is  empty 
+    if(!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
 
-
-
+      // Otherwise it adds the value to the new node.
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
   }
 
   /**
@@ -84,7 +96,7 @@ export default class DoubleLinkedList {
    */
   getNodeAtIndex(index) {
 
-       // check to make sure that the list is not empty and that the index provided is not less then 0.
+    // check to make sure that the list is not empty and that the index provided is not less then 0.
     if(!this.length || index < 0 || index>=this.length) {
       return null
     } else {
@@ -104,15 +116,49 @@ export default class DoubleLinkedList {
   /**
    * Set a node at a specific index.
    */
-  setNodeAtIndex(index, value) {}
+  setNodeAtIndex(index, value) {
+    const currentNode = this.getNodeAtIndex(index);
+
+    if(currentNode) {
+      currentNode.value = value
+    }
+    else {
+      return null;
+    }
+  }
 
   /**
    *  Insert a node at a specific index.
    */
-  insertAtIndex(index, val) {}
+  insertAtIndex(index, val) {
+    // check to make sure that the list is not empty and that the index provided is not less then 0.
+  }
 
   /**
    * Remove a node at a specific index.
    */
-  removeAtIndex(index) {}
+  removeAtIndex(index) {
+    //checks to see if it is a valid index 
+    if(!this.length || index < 0 || index >=this.length) {
+      return null;
+    } else if(index === 0) {
+      this.shift();
+    } else if(index === length - 1) {
+      this.pop();
+    } else {
+      const remove = this.getNodeAtIndex(index);
+      const preNode = remove.prev;
+      const nextNode = remove.next;
+
+      // Removes the value;
+      remove.prev = null;
+      remove.next = null;
+
+      // connects the two nodes.
+      prevNode.next = nextNode;
+      nextNode = prevNode;
+
+    }
+    this.length--;
+  }
 }
